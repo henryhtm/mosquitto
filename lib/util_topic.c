920999d24d6e1f7b06eed909ac79db76d5d4058c
dev <dev@dev.dev>
2020-08-28 14:03:59 +0800

    增加主题匹配相关代码的中文注释

--- lib/util_topic.c
+++ lib/util_topic.c
@@ -35,10 +35,13 @@ Contributors:
 #include "util_mosq.h"
 
 /* Check that a topic used for publishing is valid.
- * Search for + or # in a topic. Return MOSQ_ERR_INVAL if found.
- * Also returns MOSQ_ERR_INVAL if the topic string is too long.
- * Returns MOSQ_ERR_SUCCESS if everything is fine.
- */
+   Search for + or # in a topic. Return MOSQ_ERR_INVAL if found.
+   Also returns MOSQ_ERR_INVAL if the topic string is too long.
+   Returns MOSQ_ERR_SUCCESS if everything is fine.
+ * 检查某个发布的主题是否有效：
+   如果在主题中找到+或#字符，则返回MOSQ_ERR_INVAL；
+   如果主题过长（长度大于65535字节），也返回MOSQ_ERR_INVAL；
+   一切正常时返回成功。    */
 int mosquitto_pub_topic_check(const char *str)
 {
     int len = 0;
@@ -65,6 +68,7 @@ int mosquitto_pub_topic_check(const char *str)
     return MOSQ_ERR_SUCCESS;
 }
 
+/* 函数作用同上，通过参数len限制了字符串长度 */
 int mosquitto_pub_topic_check2(const char *str, size_t len)
 {
     size_t i;
@@ -92,12 +96,16 @@ int mosquitto_pub_topic_check2(const char *str, size_t len)
 }
 
 /* Check that a topic used for subscriptions is valid.
- * Search for + or # in a topic, check they aren't in invalid positions such as
- * foo/#/bar, foo/+bar or foo/bar#.
- * Return MOSQ_ERR_INVAL if invalid position found.
- * Also returns MOSQ_ERR_INVAL if the topic string is too long.
- * Returns MOSQ_ERR_SUCCESS if everything is fine.
- */
+   Search for + or # in a topic, check they aren't in invalid positions such as
+   foo/#/bar, foo/+bar or foo/bar#.
+   Return MOSQ_ERR_INVAL if invalid position found.
+   Also returns MOSQ_ERR_INVAL if the topic string is too long.
+   Returns MOSQ_ERR_SUCCESS if everything is fine.
+ * 检查某个订阅的主题是否有效：
+   检查主题中的+和#字符是否在非法的位置，如：foo/#/bar, foo/+bar or foo/bar#，
+   如果在非法位置则返回MOSQ_ERR_INVAL；
+   如果主题过长（长度大于65535字节），也返回MOSQ_ERR_INVAL；
+   一切正常时返回成功。    */
 int mosquitto_sub_topic_check(const char *str)
 {
     char c = '\0';
@@ -133,6 +141,7 @@ int mosquitto_sub_topic_check(const char *str)
     return MOSQ_ERR_SUCCESS;
 }
 
+/* 函数作用同上，通过参数len限制了字符串长度 */
 int mosquitto_sub_topic_check2(const char *str, size_t len)
 {
     char c = '\0';
@@ -167,12 +176,15 @@ int mosquitto_sub_topic_check2(const char *str, size_t len)
     return MOSQ_ERR_SUCCESS;
 }
 
+/* Does a topic match a subscription? 
+   检查指定主题和订阅是否匹配 */
 int mosquitto_topic_matches_sub(const char *sub, const char *topic, bool *result)
 {
     return mosquitto_topic_matches_sub2(sub, 0, topic, 0, result);
 }
 
-/* Does a topic match a subscription? */
+/* Does a topic match a subscription? 
+   检查指定主题和订阅是否匹配 */
 int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *topic, size_t topiclen, bool *result)
 {
     size_t spos;
@@ -189,48 +201,55 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
 
     if((sub[0] == '$' && topic[0] != '$')
             || (topic[0] == '$' && sub[0] != '$')){
-
         return MOSQ_ERR_SUCCESS;
     }
 
     spos = 0;
 
     while(sub[0] != 0){
+        /* 异常场景：主题中不允许存在通配符 */
         if(topic[0] == '+' || topic[0] == '#'){
             return MOSQ_ERR_INVAL;
         }
-        if(sub[0] != topic[0] || topic[0] == 0){ /* Check for wildcard matches */
+        /* 当前订阅字符和主题字符不相同，检查订阅字符是否是通配符 */
+        /* HEQ: 下面的条件语句中“|| topic[0] == 0”是否多余？ */
+        if(sub[0] != topic[0] || topic[0] == 0){
             if(sub[0] == '+'){
-                /* Check for bad "+foo" or "a/+foo" subscription */
+                /* 异常场景：当前订阅字符是"+"，但上一个订阅字符不是"/"，如："test+"，"test/he+" */
                 if(spos > 0 && sub[-1] != '/'){
                     return MOSQ_ERR_INVAL;
                 }
-                /* Check for bad "foo+" or "foo+/a" subscription */
+                /* 异常场景：当前订阅字符是"+"，但下一个订阅字符不是"/"，如："+test"，"/test/+he" */
                 if(sub[1] != 0 && sub[1] != '/'){
                     return MOSQ_ERR_INVAL;
                 }
                 spos++;
                 sub++;
+                /* 一个"+"符号匹配主题中的一个层级 */
                 while(topic[0] != 0 && topic[0] != '/'){
+                    /* 异常场景：主题中不允许存在通配符 */
                     if(topic[0] == '+' || topic[0] == '#'){
                         return MOSQ_ERR_INVAL;
                     }
                     topic++;
                 }
+                /* 匹配成功 */
                 if(topic[0] == 0 && sub[0] == 0){
                     *result = true;
                     return MOSQ_ERR_SUCCESS;
                 }
             }else if(sub[0] == '#'){
-                /* Check for bad "foo#" subscription */
+                /* 异常场景：当前订阅字符是"#"，但上一个订阅字符不是"/"，如："test#"，"test/he#" */
                 if(spos > 0 && sub[-1] != '/'){
                     return MOSQ_ERR_INVAL;
                 }
-                /* Check for # not the final character of the sub, e.g. "#foo" */
+                /* 异常场景："#"字符不是订阅中的最后一个字符，如："test/#foo" */
                 if(sub[1] != 0){
                     return MOSQ_ERR_INVAL;
                 }else{
+                    /* 订阅中的"#"字符匹配主题中剩下的所有内容 */
                     while(topic[0] != 0){
+                        /* 异常场景：主题中不允许存在通配符 */
                         if(topic[0] == '+' || topic[0] == '#'){
                             return MOSQ_ERR_INVAL;
                         }
@@ -239,19 +258,17 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
                     *result = true;
                     return MOSQ_ERR_SUCCESS;
                 }
-            }else{
-                /* Check for e.g. foo/bar matching foo/+/# */
-                if(topic[0] == 0
-                        && spos > 0
-                        && sub[-1] == '+'
-                        && sub[0] == '/'
-                        && sub[1] == '#')
+            }else{ /* 订阅中的当前字符不是“+”和“#” */
+                /* 特殊场景：主题字符串结束但订阅中的当前字符是"/"，并且上一个字符是"+"，下一个字符是"#"，
+                   如："test/+/#" ，这种情况下和主题"test/go"可以匹配成功 */
+                if(topic[0] == 0 && spos > 0
+                   && sub[-1] == '+' && sub[0] == '/' && sub[1] == '#')
                 {
                     *result = true;
                     return MOSQ_ERR_SUCCESS;
                 }
 
-                /* There is no match at this point, but is the sub invalid? */
+                /* 订阅与主题不匹配，检查订阅中的每个字符来确认订阅是否有效 */
                 while(sub[0] != 0){
                     if(sub[0] == '#' && sub[1] != 0){
                         return MOSQ_ERR_INVAL;
@@ -260,13 +277,13 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
                     sub++;
                 }
 
-                /* Valid input, but no match */
+                /* 订阅的主题有效但匹配不成功 */
                 return MOSQ_ERR_SUCCESS;
             }
-        }else{
-            /* sub[spos] == topic[tpos] */
+        }else{ /* 当前订阅字符和主题字符相同 */
             if(topic[1] == 0){
-                /* Check for e.g. foo matching foo/# */
+                /* 特殊场景：主题字符串结束但订阅字符串多包含"/#"两个字符。
+                   例如： 主题"test"和订阅"test/#"匹配    */
                 if(sub[1] == '/'
                         && sub[2] == '#'
                         && sub[3] == 0){
@@ -280,8 +297,8 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
             if(sub[0] == 0 && topic[0] == 0){
                 *result = true;
                 return MOSQ_ERR_SUCCESS;
-            }else if(topic[0] == 0 && sub[0] == '+' && sub[1] == 0){
-                if(spos > 0 && sub[-1] != '/'){
+            }else if(topic[0] == 0 && sub[0] == '+' && sub[1] == 0){ /* 主题字符串已经结束，订阅字符串最后字符为"+" */
+                if(spos > 0 && sub[-1] != '/'){ /* 异常场景：订阅中的格式无效，如："test+" */
                     return MOSQ_ERR_INVAL;
                 }
                 spos++;
@@ -290,16 +307,17 @@ int mosquitto_topic_matches_sub2(const char *sub, size_t sublen, const char *top
                 return MOSQ_ERR_SUCCESS;
             }
         }
-    }
+    }    /* 订阅字符串结束 */
+    
     if((topic[0] != 0 || sub[0] != 0)){
         *result = false;
     }
     while(topic[0] != 0){
+        /* 异常场景：主题中不允许存在通配符 */
         if(topic[0] == '+' || topic[0] == '#'){
             return MOSQ_ERR_INVAL;
         }
         topic++;
     }
-
     return MOSQ_ERR_SUCCESS;
 }
