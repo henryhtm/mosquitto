c41b4b1b6b0929acb17656be1ac9842edcb52f86
dev <dev@dev.dev>
2020-09-04 15:11:00 +0800

    修改mosquitto__free函数，解决MEMORY_TRACKING宏未打开时会free空指针的问题

--- lib/memory_mosq.c
+++ lib/memory_mosq.c
@@ -68,10 +68,10 @@ void *mosquitto__calloc(size_t nmemb, size_t size)
 
 void mosquitto__free(void *mem)
 {
-#ifdef REAL_WITH_MEMORY_TRACKING
     if(!mem){
         return;
     }
+#ifdef REAL_WITH_MEMORY_TRACKING
     memcount -= malloc_usable_size(mem);
 #endif
     free(mem);
